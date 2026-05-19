5fcba4f07e2a6df6b87d59cd0616d8b1ce779745
dieruki <stapps90@gmail.com>
2026-05-19 15:51:27 +0300

    fix price + fix x overflow page on mobile

--- script.js
+++ script.js
@@ -256,7 +256,7 @@ document.addEventListener('click', (e) => {
     if (hexEl) hexEl.textContent = hex.toUpperCase();
     if (rgbEl) rgbEl.textContent = `${rgb.r}, ${rgb.g}, ${rgb.b}`;
 
-    orderBtn.setAttribute('data-offers', `[{"article":2,"name":"Большой котик 25см (${hex.toUpperCase()})","count":1,"price":4999,"weight":1,"image":"assets/cat-pink.png"}]`);
+    orderBtn.setAttribute('data-offers', `[{"article":2,"name":"Большой котик 25см (${hex.toUpperCase()})","count":1,"price":4387,"weight":1,"image":"assets/cat-pink.png"}]`);
   };
 
   const open = () => {
